04896de1e1bef121ef759c95c464d1f8c9916fcd
Yonass12 <157059976+Yonass12@users.noreply.github.com>
2024-02-25 21:50:57 +0100

    Update audio-tomp3.js

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -1,16 +1,85 @@
-import {toAudio} from '../lib/converter.js';
-const handler = async (m, {conn, usedPrefix, command}) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q || q.msg).mimetype || q.mediaType || '';
-  if (!/video|audio/.test(mime)) throw `*هذا الامر خاص بتحويل مقطع فيديو لموسيقى*`;
-  const media = await q.download();
-  if (!media) throw '*تأكد انك قمت بارسال فيديو وتأكد انك كتبت \n.tomp3*';
-  const audio = await toAudio(media, 'mp4');
-  if (!audio.data) throw '*حذث خطأ راسل صاحب البوت*';
-  conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
-};
-;
-handler.help = ["tomp3"]
-handler.tags = ["tools"]
-handler.command = /^(tos)$/i
-export default handler;
+const fs = require('fs')
+const path = require('path')
+const { spawn } = require('child_process')
+
+function ffmpeg(buffer, args = [], ext = '', ext2 = '') {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let tmp = path.join(__dirname, '../src', + new Date + '.' + ext)
+      let out = tmp + '.' + ext2
+      await fs.promises.writeFile(tmp, buffer)
+      spawn('ffmpeg', [
+        '-y',
+        '-i', tmp,
+        ...args,
+        out
+      ])
+        .on('error', reject)
+        .on('close', async (code) => {
+          try {
+            await fs.promises.unlink(tmp)
+            if (code !== 0) return reject(code)
+            resolve(await fs.promises.readFile(out))
+            await fs.promises.unlink(out)
+          } catch (e) {
+            reject(e)
+          }
+        })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+/**
+ * Convert Audio to Playable WhatsApp Audio
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension 
+ */
+function toAudio(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-vn',
+    '-ac', '2',
+    '-b:a', '128k',
+    '-ar', '44100',
+    '-f', 'mp3'
+  ], ext, 'mp3')
+}
+
+/**
+ * Convert Audio to Playable WhatsApp PTT
+ * @param {Buffer} buffer Audio Buffer
+ * @param {String} ext File Extension 
+ */
+function toPTT(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-vn',
+    '-c:a', 'libopus',
+    '-b:a', '128k',
+    '-vbr', 'on',
+    '-compression_level', '10'
+  ], ext, 'opus')
+}
+
+/**
+ * Convert Audio to Playable WhatsApp Video
+ * @param {Buffer} buffer Video Buffer
+ * @param {String} ext File Extension 
+ */
+function toVideo(buffer, ext) {
+  return ffmpeg(buffer, [
+    '-c:v', 'libx264',
+    '-c:a', 'aac',
+    '-ab', '128k',
+    '-ar', '44100',
+    '-crf', '32',
+    '-preset', 'slow'
+  ], ext, 'mp4')
+}
+
+module.exports = {
+  toAudio,
+  toPTT,
+  toVideo,
+  ffmpeg,
+} 
